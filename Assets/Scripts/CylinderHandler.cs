28d422c56f19876bfb8a64477a423aaaa3570109
Martin <martinporthouse@hotmail.com>
2024-10-29 14:15:47 +0100

    Dimensioner på kasse og cylinder

--- Assets/Scripts/CylinderHandler.cs
+++ Assets/Scripts/CylinderHandler.cs
@@ -6,18 +6,36 @@ using UnityEngine.UI;
 
 public class CylinderHandler : MonoBehaviour
 {
+    public float buffer = 0;
+
+    public GameObject cylinder, worldCanvas;
+
     public Slider radiusSlider, heightSlider;
     public TextMeshProUGUI volumeDisplay, surfaceAreaDisplay;
+    public TextMeshProUGUI radiusText, heightText;
 
     private float radius = 0.5f, height = 1;
     private float volume, surfaceArea;
 
     void Start()
     {
-        radiusSlider.onValueChanged.AddListener((value) => { UpdateValues(value, "radius"); });
-        heightSlider.onValueChanged.AddListener((value) => { UpdateValues(value, "height"); });
+        SetupWorldCanvas();
+        SetupSliders();
         UpdateDimensions();
         UpdateUI();
+    }
+
+    void SetupWorldCanvas()
+    {
+        worldCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
+        worldCanvas.GetComponent<RectTransform>().sizeDelta = cylinder.transform.localScale + Vector3.one * 0.4f;
+        worldCanvas.transform.rotation = cylinder.transform.rotation;
+    }
+
+    void SetupSliders()
+    {
+        radiusSlider.onValueChanged.AddListener((value) => { UpdateValues(value, "radius"); });
+        heightSlider.onValueChanged.AddListener((value) => { UpdateValues(value, "height"); });
     }
 
     void UpdateDimensions()
@@ -25,7 +43,10 @@ public class CylinderHandler : MonoBehaviour
         volume = height * Mathf.PI * Mathf.Pow(radius, 2f);
         surfaceArea = 2 * Mathf.PI * radius * (height + radius);
 
-        transform.localScale = new Vector3(radius * 2, height, radius * 2);
+        cylinder.transform.localScale = new Vector3(radius * 2, height, radius * 2);
+
+        radiusText.transform.position = new Vector3(radius / 2, height + buffer, 0);
+        heightText.transform.position = new Vector3(radius + buffer, 0, 0);
     }
 
     void UpdateUI()
@@ -33,12 +54,12 @@ public class CylinderHandler : MonoBehaviour
         string volumeText = volume.ToString("0.00"),
             surfaceAreaText = surfaceArea.ToString("0.00");
 
-        string radiusText = radius.ToString("0.00"),
-            heightText = height.ToString("0.00");
+        radiusText.text = radius.ToString("0.00");
+        heightText.text = height.ToString("0.00");
 
-        volumeDisplay.text = $"Cylenderets volumen:\nV = h * π * r^2\n{heightText} * π * {radiusText}^2 = {volumeText}";
+        volumeDisplay.text = $"Cylenderets volumen:\nV = h * π * r^2\n{heightText.text} * π * {radiusText.text}^2 = {volumeText}";
 
-        surfaceAreaDisplay.text = $"Cylenderets overfladeareal:\nA = 2 * π * r * (h + r)\n2 * π * {radiusText} * ({heightText} + {radiusText}) = {surfaceAreaText}";
+        surfaceAreaDisplay.text = $"Cylenderets overfladeareal:\nA = 2 * π * r * (h + r)\n2 * π * {radiusText.text} * ({heightText.text} + {radiusText.text}) = {surfaceAreaText}";
     }
 
     void UpdateValues(float value, string dimension)
