10889c6d8b429401bf81a03c88d645d25f3ba444
Martin <martinporthouse@hotmail.com>
2024-11-08 09:56:56 +0100

    Shapes work

--- Assets/Scripts/SphereHandler.cs
+++ Assets/Scripts/SphereHandler.cs
@@ -6,17 +6,35 @@ using UnityEngine.UI;
 
 public class SphereHandler : MonoBehaviour
 {
+    public float buffer = 0;
+
+    public GameObject sphere, worldCanvas;
+
     public Slider radiusSlider;
     public TextMeshProUGUI volumeDisplay, surfaceAreaDisplay;
+    public TextMeshProUGUI radiusText;
 
     private float radius = 1;
     private float volume, surfaceArea;
 
     void Start()
     {
-        radiusSlider.onValueChanged.AddListener((value) => { UpdateValues(value); });
+        SetupWorldCanvas();
+        SetupSliders();
         UpdateDimensions();
         UpdateUI();
+    }
+
+    void SetupWorldCanvas()
+    {
+        worldCanvas.GetComponent<Canvas>().worldCamera = Camera.main;
+        worldCanvas.GetComponent<RectTransform>().sizeDelta = sphere.transform.localScale + Vector3.one * 0.4f;
+        worldCanvas.transform.rotation = sphere.transform.rotation;
+    }
+
+    void SetupSliders()
+    {
+        radiusSlider.onValueChanged.AddListener((value) => { UpdateValues(value); });
     }
 
     void UpdateDimensions()
@@ -24,7 +42,9 @@ public class SphereHandler : MonoBehaviour
         volume = 4f/3f * Mathf.PI * Mathf.Pow(radius, 3);
         surfaceArea = 4 * Mathf.PI * Mathf.Pow(radius, 2);
 
-        transform.localScale = new Vector3(radius, radius, radius);
+        sphere.transform.localScale = new Vector3(radius, radius, radius);
+
+        radiusText.transform.position = new Vector3(radius / 2 + buffer, 0, 0);
     }
 
     void UpdateUI()
@@ -32,11 +52,11 @@ public class SphereHandler : MonoBehaviour
         string volumeText = volume.ToString("0.00"),
             surfaceAreaText = surfaceArea.ToString("0.00");
 
-        string radiusText = radius.ToString("0.00");
+        radiusText.text = radius.ToString("0.00");
 
-        volumeDisplay.text = $"Kuglens volumen: V = 4 / 3 * π * r^3\n3 / 4 * π * {radiusText}^3 = {volumeText}";
+        volumeDisplay.text = $"Kuglens volumen: V = 4 / 3 * π * r^3\n3 / 4 * π * {radiusText.text}^3 = {volumeText}";
 
-        surfaceAreaDisplay.text = $"Kuglens overfladeareal:\nA = 4 * π * r^2\n4 * π * {radiusText}^2 = {surfaceAreaText}";
+        surfaceAreaDisplay.text = $"Kuglens overfladeareal:\nA = 4 * π * r^2\n4 * π * {radiusText.text}^2 = {surfaceAreaText}";
     }
 
     void UpdateValues(float value)
