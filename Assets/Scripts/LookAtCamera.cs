6bd6edfa1fa548b3e14cde8e3ac1bbe7fc8e0545
Mads Jensen <madsj20@student.sdu.dk>
2024-11-22 09:30:57 +0100

    Fixed LookAtCamera
    Numbers now look at the camera at the right orientation

--- Assets/Scripts/LookAtCamera.cs
+++ Assets/Scripts/LookAtCamera.cs
@@ -14,6 +14,6 @@ public class LookAtCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(transform.position + (transform.position - Camera.main.transform.position));
     }
 }
